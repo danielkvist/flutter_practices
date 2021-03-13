bad711242376f6efe9a6e4607a973b8e0d1a7518
danielkvist <daniel@dkvist.com>
2021-03-13 17:30:11 +0100

    analog_clock: add appbar widgets

--- analog_clock/lib/screens/home_screen.dart
+++ analog_clock/lib/screens/home_screen.dart
@@ -3,6 +3,43 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.settings_input_component),
+        color: Theme.of(context).iconTheme.color,
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        buildAddButton(context),
+      ],
+    );
+  }
+
+  Padding buildAddButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
