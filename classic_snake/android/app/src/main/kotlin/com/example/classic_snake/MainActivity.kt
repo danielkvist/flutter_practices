f46aa0d411e5fda4409ec9c63651f209ab483ddb
danielkvist <daniel@dkvist.com>
2021-03-18 19:58:41 +0100

    snake: init commit

---
+++ classic_snake/android/app/src/main/kotlin/com/example/classic_snake/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.classic_snake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ classic_snake/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Classic Snake',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Container(),
+        ));
+  }
+}
